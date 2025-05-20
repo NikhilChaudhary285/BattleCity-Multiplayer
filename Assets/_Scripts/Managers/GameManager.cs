097c8a771f61aaafbd177b820baf32819f9a9e38
NikhilChaudhary285 <nikhilchaudhary285@gmail.com>
2025-05-20 10:09:12 +0530

    Setup scene, player prefab, GameManager singleton.

---
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this) Destroy(gameObject);
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void InitializeGame()
+    {
+        // We'll spawn tanks here later
+    }
+}
