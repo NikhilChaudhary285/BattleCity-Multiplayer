9fce977bd4e592af1702a8ad44d7da96e32adf15
Nikhil <nikhil@deftsoft.info>
2025-06-19 16:52:08 +0530

    Added PhotonManager with clean Singleton setup and ScriptableObject-based config for versioned multiplayer connection

---
+++ Assets/_Scripts/Networking/Photon/PhotonManager.cs
@@ -0,0 +1,62 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+	public static PhotonManager Instance { get; private set; }
+
+	[Header("Photon Config")]
+	[SerializeField] private PhotonAppConfig config;
+
+	public bool IsConnectedToMaster => PhotonNetwork.IsConnectedAndReady;
+
+	private void Awake()
+	{
+		if (Instance != null && Instance != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		Instance = this;
+		DontDestroyOnLoad(this);
+	}
+
+	private void Start()
+	{
+		ConnectToPhotonServer();
+	}
+
+	public void ConnectToPhotonServer()
+	{
+		PhotonNetwork.AutomaticallySyncScene = true;
+		PhotonNetwork.GameVersion = config.gameVersion;
+
+		PhotonNetwork.ConnectUsingSettings();
+
+		Debug.Log($"Connecting to Photon... [v{config.gameVersion}]");
+	}
+
+	public override void OnConnectedToMaster()
+	{
+		Debug.Log("✅ Connected to Photon Master Server");
+
+		JoinLobby();
+	}
+
+	public override void OnDisconnected(DisconnectCause cause)
+	{
+		Debug.LogWarning($"❌ Disconnected from Photon: {cause}");
+	}
+
+	public override void OnJoinedLobby()
+	{
+		Debug.Log("🎉 Joined Photon Lobby");
+	}
+
+	private void JoinLobby()
+	{
+		PhotonNetwork.JoinLobby();
+	}
+}
